1193092f426e9ea7135df12862c5d01d2beaac05
Rodrigo Cruz Martinez <91926779+kikooooo22@users.noreply.github.com>
2024-09-21 21:50:02 -0600

    Parcial 1 - Add Async code

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -18,18 +19,27 @@ public class UsersController : ControllerBase
     }
 
 
-    [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers(){
-        var users = _context.Users.ToList();
-        return Ok(users); 
+    [HttpGet] // api/v1/users
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsersAsync(){
+        
+        var users = await _context.Users.ToListAsync();
+        
+        return users; 
     }
 
-    [HttpGet("{id}")]
-    public ActionResult<AppUser> GetUsersById(int id){
-        var user = _context.Users.Find(id);
+    [HttpGet("{id:int}")]// api/v1/users/2
+    public async Task<ActionResult<AppUser>> GetUsersByIdAsync(int id){
+        
+        var user = await _context.Users.FindAsync(id);
 
         if (user == null) return NotFound(); 
 
         return user; 
+    }
+
+    [HttpGet("{name}")] // api/v1/users/Patricio
+    public ActionResult<string> Ready(string name){
+
+        return $"Hola {name}"; 
     }
 }
