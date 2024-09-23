704bc606ff2889b5949ea0521f0708e507a01cbb
Rodrigo Cruz Martinez <rodrigocruzmtz09@gmail.com>
2024-09-22 18:05:20 -0600

    Parcial 1 - Install Angular (reinstall)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,13 @@
-import { NgFor } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
-  http = inject(HttpClient);
-  title = 'Coco';
-  users: any; 
-
-
-  ngOnInit(): void {
-    this.http.get("https://localchost:5001/api/v1/users").subscribe({
-      next: (response) => {this.users = response},
-      error: (error) => {console.log(error)},
-      complete: () => {console.log("Request completed")}
-    }); 
-  }
-  
+export class AppComponent {
+  title = 'client';
 }
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,18 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient() // Permite inyectar angular en diferentes partes 
-    /* 
-    Todos soportan inyeccion de dependencias (angular)
-    No instanciar una clase directamente, sino se le dice al codigo 
-    que naturalmente debe incluirlo 
-    Se basa en los 5 'SOLID Principles'  
-    Esto es bueno pues el codigo es mas independiente, mas mantenible, etc.
-    */
-  ]
+  providers: [provideRouter(routes)]
 };
