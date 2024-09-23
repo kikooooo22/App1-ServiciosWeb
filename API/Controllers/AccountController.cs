4f26265047d9b3ef60476ff2886a3c589e91c0ce
Rodrigo Cruz Martinez <rodrigocruzmtz09@gmail.com>
2024-09-23 04:27:49 -0600

    Parcial 1 -Add DTOs

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,22 +1,29 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AccountController(DataContext context) : BaseApiController
 {
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> RegisterAsync(string username, string password){
+    public async Task<ActionResult<AppUser>> RegisterAsync(RegisterRequest request){
         
+        if ( await UserExisteAsync(request.Username))
+        {
+            return BadRequest("Username already exist");
+        } 
+
         using var hmac = new HMACSHA512();
 
         // using: Una vez se ejecuta, se libera la memoria 
         var user = new AppUser{
-            UserName = username,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            UserName = request.Username,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password)),
             PasswordSalt = hmac.Key
         };
 
@@ -25,4 +32,7 @@ public class AccountController(DataContext context) : BaseApiController
 
         return user;
     }
+
+    private async Task<bool> UserExisteAsync(string username) =>
+        await context.Users.AnyAsync(user => user.UserName.ToLower() == username.ToLower());
 }
