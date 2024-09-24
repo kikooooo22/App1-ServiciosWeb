f53aa3930c9fadeda2441ac98c5e438352024432
Rodrigo Cruz Martinez <rodrigocruzmtz09@gmail.com>
2024-09-24 00:18:08 -0600

    Parcial 1 - Add Extension Methods

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using API.Data;
+using API.Extensions;
 using API.Interfaces;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -9,30 +10,9 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors();
-
-// En donde encuentre 'ITokenService' lo remplaza por (injecta) 'TokenService'
-builder.Services.AddScoped<ITokenService, TokenService>(); 
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options => 
-    {
-        var tokenKey = builder.Configuration["TokenKey"] ?? throw new ArgumentNullException("TokenKey");
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
-            ValidateIssuer = false, 
-            ValidateAudience = false
-        };
-    });
+// Haciendo uso de extensiones, 
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration); 
 
 var app = builder.Build();
 
