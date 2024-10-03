470cf449d73dfdded34d6a90b5ab6a0070a8b486
Rodrigo Cruz Martinez <91926779+kikooooo22@users.noreply.github.com>
2024-10-03 07:53:24 -0600

    Parcial 1 - Add user registration

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -10,12 +11,21 @@ import { FormsModule } from '@angular/forms';
 })
 export class RegisterComponent {
 
-  usersFromHomeComponent = input.required<any>(); 
+  private accounService = inject(AccountService);
+  //usersFromHomeComponent = input.required<any>(); 
   cancelRegister = output<boolean>(); 
   model: any = {}; 
 
   register(): void{
-    console.log(this.model); 
+    this.accounService.register(this.model).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.cancel();
+      },
+      error: (error) =>{
+        console.log(error);
+      }
+    });
   }
 
   cancel(): void{
