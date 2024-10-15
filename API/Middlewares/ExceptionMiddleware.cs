8168855ad4c569e14abd30e213e66aab2606034e
Rodrigo Cruz Martinez <rodrigocruzmtz09@gmail.com>
2024-10-14 22:40:41 -0600

    Parcial 2 - Se me olvido guardar un archivo jaja salu2

--- API/Middlewares/ExceptionMiddleware.cs
+++ API/Middlewares/ExceptionMiddleware.cs
@@ -1,6 +1,32 @@
 namespace API.Middlewares;
-
-public class ExceptionMiddleware
+using System.Net;
+using System.Text.Json;
+using API.Errors;
+public class ExceptionMiddleware(
+    RequestDelegate next,
+    ILogger<ExceptionMiddleware> logger,
+    IHostEnvironment env)
 {
-    
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            var response = env.IsDevelopment()
+                ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace)
+                : new ApiException(context.Response.StatusCode, ex.Message, "Internal Server Error");
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            var json = JsonSerializer.Serialize(response, options);
+            await context.Response.WriteAsync(json);
+        }
+    }
 }
