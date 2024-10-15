6bb059e99cee72a377c25716dab10712fd37326d
Rodrigo Cruz Martinez <rodrigocruzmtz09@gmail.com>
2024-10-14 21:39:25 -0600

    Parcial 2 - Fix formating

--- API/DTOs/LoginRequest.cs
+++ API/DTOs/LoginRequest.cs
@@ -3,6 +3,5 @@ namespace API.DTOs;
 public class LoginRequest
 {
     public required string Username { get; set; }
-    public required string Password { get; set;}
-    
+    public required string Password { get; set; }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,18 +1,10 @@
-using System.Text;
-using API.Data;
 using API.Extensions;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Haciendo uso de extensiones, 
 builder.Services.AddApplicationServices(builder.Configuration);
-builder.Services.AddIdentityServices(builder.Configuration); 
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
