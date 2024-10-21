cd6e7a0aa94545021ad1f6e190b6181f951d8c20
Rodrigo Cruz Martinez <rodrigocruzmtz09@gmail.com>
2024-10-21 01:14:42 -0600

    Parcial 2 - ADELANTO - Change context to repository

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,33 +3,31 @@ using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 
 [Authorize]
 public class UsersController : BaseApiController
 {
 
-    private readonly DataContext _context;
+    private readonly IUserRepository _repository;
 
-    public UsersController(DataContext context)
+    public UsersController(UserRepository repository)
     {
-        _context = context;
+        _repository = repository;
     }
 
-    [AllowAnonymous]
     [HttpGet] // api/users
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsersAsync()
     {
-        var users = await _context.Users.ToListAsync();
-        return users;
+        var users = await _repository.GetAllAsync();
+        return Ok(users);
     }
 
-    [Authorize]
     [HttpGet("{id:int}")]// api/users/2
-    public async Task<ActionResult<AppUser>> GetUsersByIdAsync(int id)
+    public async Task<ActionResult<AppUser>> GetByIdAsync(int id)
     {
-        var user = await _context.Users.FindAsync(id);
+        var user = await _repository.GetByIdAsync(id);
+
         if (user == null)
         {
             return NotFound();
@@ -38,6 +36,16 @@ public class UsersController : BaseApiController
         return user;
     }
 
-    [HttpGet("{name}")] // api/users/Patricio
-    public ActionResult<string> Ready(string name) => $"Hi {name}";
+    [HttpGet("{username}")] // api/users/Patricio
+    public async Task<ActionResult<AppUser>> GetByUsernameAsync(string username)
+    {
+        var user = await _repository.GetByUsernameAsync(username);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return user;
+    }
 }
