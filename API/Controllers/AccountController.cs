2b393ef1fa8e2452bc20ecdfe9ecd1b25ca0c5d2
Rodrigo Cruz Martinez <rodrigocruzmtz09@gmail.com>
2024-10-20 22:29:59 -0600

    Parcial 2 - Add new props to AppUser and create DateTime extension method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -22,25 +22,27 @@ public class AccountController(
             return BadRequest("Username already exist");
         }
 
-        // Se crea el salt (No olvidemos que es objeto para hashear)
-        using var hmac = new HMACSHA512();
+        return Ok();
 
-        // using: Una vez se ejecuta, se libera la memoria 
-        var user = new AppUser
-        {
-            UserName = request.Username,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password)),
-            PasswordSalt = hmac.Key
-        };
+        // // Se crea el salt (No olvidemos que es objeto para hashear)
+        // using var hmac = new HMACSHA512();
 
-        context.Users.Add(user);
-        await context.SaveChangesAsync();
+        // // using: Una vez se ejecuta, se libera la memoria 
+        // var user = new AppUser
+        // {
+        //     UserName = request.Username,
+        //     PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password)),
+        //     PasswordSalt = hmac.Key
+        // };
 
-        return new UserResponse
-        {
-            Username = user.UserName,
-            Token = tokenService.CreateToken(user)
-        };
+        // context.Users.Add(user);
+        // await context.SaveChangesAsync();
+
+        // return new UserResponse
+        // {
+        //     Username = user.UserName,
+        //     Token = tokenService.CreateToken(user)
+        // };
 
     }//RegisterAsync
 
