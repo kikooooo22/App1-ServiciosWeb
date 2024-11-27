9c6541d35f3ba3a1bea4b027e4326d1ea48d417c
Rodrigo Cruz Martinez <rodrigocruzmtz09@gmail.com>
2024-11-27 00:21:31 -0600

    Parcial 03 - Update member update in SPA

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -6,12 +6,19 @@ import { Member } from '../_models/member';
   providedIn: 'root'
 })
 export class MembersService {
+
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
+
   getMembers() {
     return this.http.get<Member[]>(this.baseUrl + "users");
   }
+
   getMember(username: string) {
     return this.http.get<Member>(this.baseUrl + "users/" + username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + "users", member);
   }
 }
